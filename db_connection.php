31214828158f3e9a818f81fb5e933919d2c555ed
Diti Nitin Divekar <79137306+DITI2209@users.noreply.github.com>
2022-11-17 09:08:03 +0530

    Delete db_connection.php

--- db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_connection = mysqli_connect("localhost","root","","login_registration");
-// Check connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
